63e75443058210963f9f6060b340c2ea860b4589
ogodwin <oliver.godwin3414@omail.otech.edu>
2022-07-30 17:36:55 -0600

    Created ISavable and ISearchable. Made entity and inventory item serializable

--- CKK.Logic/Interfaces/Entity.cs
+++ CKK.Logic/Interfaces/Entity.cs
@@ -8,6 +8,7 @@ using CKK.Logic.Exceptions;
 
 namespace CKK.Logic.Interfaces
 {
+    [Serializable]
     public abstract class Entity
     {
         private int _Id;
---
+++ CKK.Logic/Interfaces/ISavable.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Linq;
+using CKK.Logic.Models;
+using CKK.Logic.Interfaces;
+
+namespace CKK.Logic.Interfaces {
+
+    public interface ISavable 
+    {
+
+    }
+}
